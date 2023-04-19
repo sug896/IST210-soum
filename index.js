e2b3144d6bc9fa9ee16a4df62bf13f235968b4c8
sug896 <122576599+sug896@users.noreply.github.com>
2023-04-19 14:39:09 -0400

    Update index.js
    removed redundant code

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ var query2 = 'INSERT INTO `student` (`fName`, `lName`, `StudentID`, `email`) VAL
 connecttion.connect();
 
 app.post('/createuser', function(request, res, next){
-    var profile = JSON.parse(request);
+    var profile = JSON.parse(request.body);
     var query = 'INSERT INTO `student` (`fName`, `lName`, `StudentID`, `email`) VALUES'; 
-    query += '('+profile.fName+', ' + profile.lName+', "0000003", "sally896@psu.edu")';
+    query += '('+profile.fName+', ' + profile.lName +', "0000003", "sally896@psu.edu")';
     connecttion.query(query2, function(error, response, fields){
     if (error) throw error;
         res.send(response);
@@ -31,5 +31,3 @@ app.post('/createuser', function(request, res, next){
 app.listen(port, function(){
     console.log('http://localhost:'+port)
 })
-
-//https://console.pingone.com/index.html?env=024c120b-895d-4b97-986f-e883c2e47515#/a349b0aa-c57c-4c9c-a047-8f261441ef93/overviewDashboard
