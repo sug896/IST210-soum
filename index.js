491be293852b613497fca4f83447bd29d6743ffb
sug896 <122576599+sug896@users.noreply.github.com>
2023-04-23 14:15:37 -0400

    Update index.js
    added /order/create and /order as a landing page

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ var connecttion = mysql.createConnection({
 })
 var query1 = 'SELECT * FROM student';
 var query2 = 'INSERT INTO `student` (`fName`, `lName`, `StudentID`, `email`) VALUES ("sally", "smith", "0000003", "sally896@psu.edu")';
-connecttion.connect();
 
 app.post('/createuser', function(request, res, next){
     console.log(request.body)
@@ -28,6 +27,57 @@ app.post('/createuser', function(request, res, next){
         res.send(response);
     })
    
+})
+
+function orderForm(message){
+    return `<!DOCTYPE html>
+    <html>
+    <head>
+    <title>web app</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </head>
+    
+        <body>
+            <div class="container">
+               <h1>Enter your order here</h1>
+                <form action="/order/create" method="POST">
+                <div class="row">
+                    <div class="col-md">
+                        <input class="form-control" type="number" name="itemNumber" placeholder="item number"/>
+                    </div>
+                    <div class="col-md">
+                        <input class="form-control" type="number" name="itemQuantity" placeholder="quantity"/>
+                    </div>
+                    <div class="col-sm">
+                        <input type="submit" class="btn btn-primary" value="submit">
+                    </div>
+                </form>
+                <div class="col-lg">
+                    ${message? 'Thank you for your order' : ''}
+                </div>
+            </div>
+        </body>
+    </html>`
+}
+
+app.get('/order', function(request, res, next){
+    res.send(orderForm(request.query.sucess));
+})
+
+app.post('/order/create', function(req, res, next){
+    var qty = req.body.itemQuantity;
+    var itemCode = req.body.itemNumber;
+    connecttion.connect(function(error, response){
+        if (error) throw error;
+        var query = `UPDATE items SET itemQuantity = ( itemQuantity -` + qty +`) WHERE itemCode = ` + itemCode;
+        connecttion.query(query, function(error, response, fields){
+            if (error) throw error;
+            if(response.changedRows == 1){
+                connecttion.end();
+                res.redirect('/order?sucess=true');
+            }
+        });
+    });
 })
 
 app.listen(port, function(){
